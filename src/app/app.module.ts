600836bdc8252c13d4c00772d0891f21179aed03
Ronny <mwithiamoki@gmail.com>
2020-08-25 08:50:18 +0300

    initial commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { HighlightDirective } from './highlight.directive';
+import { PostTimePipe } from './post-time.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { MomentModule } from 'ngx-moment';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent
+    QuoteComponent,
+    QuoteDetailComponent,
+    HighlightDirective,
+    PostTimePipe,
+    QuoteFormComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    FormsModule,
+    MomentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/post-time.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PostTimePipe } from './post-time.pipe';
+
+describe('PostTimePipe', () => {
+  it('create an instance', () => {
+    const pipe = new PostTimePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
