69d5fd54ff866b6125ed47d0a138990032586066
Ronny <mwithiamoki@gmail.com>
2020-08-26 23:43:16 +0300

    updates

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,7 +12,7 @@ export class QuoteComponent implements OnInit {
     new Quote('Confucius','To See What Is Right And Not Do It Is A Lack Of Courage',new Date(1943,12,1)),
     new Quote(' Aristotle','Happiness depends upon ourselves',new Date(2016,9,12)),
     new Quote(' Seneca','It is more fitting for a man to laugh at life than to lament over it',new Date(1918,6,12)),
-    new Quote(' Arthur Schopenhauer','The two enemies of human happiness are pain and boredom',new Date(1919,10,18)),
+    new Quote('  Schopenhauer','The two enemies of human happiness are pain and boredom',new Date(1919,10,18)),
     ];
     addNewQuote(quote){
       quote.completeDate = new Date(quote.completeDate)
