8aa25a3adc264519a842debd9178851d5730c722
Ronny <mwithiamoki@gmail.com>
2020-08-27 12:12:47 +0300

    Auto-generated commit

--- src/app/post-time.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PostTimePipe } from './post-time.pipe';
-
-describe('PostTimePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PostTimePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
